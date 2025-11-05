c3472176f1991d63235e258fd851cc9d9ce3d09e
youcefsaker <youcefsaker201@gamil.com>
2025-11-05 21:50:12 +0100

    Correction de la types-interactions/index.blade.php et /users/index.blade.php

--- resources/views/types-interactions/index.blade.php
+++ resources/views/types-interactions/index.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app :title="__('Types d\'interactions')">
-    <x-slot name="header">
+
         <div class="flex items-center justify-between">
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Types d\'interactions') }}
@@ -7,7 +7,7 @@
             
            
         </div>
-    </x-slot>
+
 
     @include('components.flash-messages')
 
@@ -23,10 +23,7 @@
     }" class="py-6">
         <!-- Ajout d'un bouton de test comme dans users -->
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4">
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                Types d'interactions
-            </h2>
-            <div class="mt-4"></div>
+           
             <button 
                 @click="showCreateModal = true"
                 class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors duration-150 shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
